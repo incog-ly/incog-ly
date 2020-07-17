8c2f31421a2c9fc2cc9bf9f4f136ec99299e64fc
Aniket Kumar <aniketkumar049@gmail.com>
2020-07-18 03:38:25 +0530

    final commit

--- conf/setup.sh
+++ conf/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-PROJECT_BASE_PATH='/root/half-mile-hackathon'
+PROJECT_BASE_PATH='/half-mile-hackathon'
 # Configure nginx
-cp $PROJECT_BASE_PATH/deploy/nginx_half-mile-hackathon.conf /etc/nginx/sites-available/half-mile-hackathon.conf
+cp $PROJECT_BASE_PATH/conf/nginx_half-mile-hackathon.conf /etc/nginx/sites-available/half-mile-hackathon.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/half-mile-hackathon.conf /etc/nginx/sites-enabled/half-mile-hackathon.conf
 systemctl restart nginx.service
